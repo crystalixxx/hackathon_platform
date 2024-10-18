93dfefa2d58eb1f0ff05fabf065874b6a7d288dd
Maxim Drugov <midrugov@edu.hse.ru>
2024-10-18 10:56:05 +0300

    schemas filled by users classes

---
+++ backend/app/database/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: str
+    first_name: str
+    second_name: str
+    hashed_password: str
+    is_admin: bool = False
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserEdit(UserBase):
+    password: str | None = None
+
+
+class User(UserBase):
+    id: int
