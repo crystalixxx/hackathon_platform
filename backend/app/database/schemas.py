656f44bfbcb8e14edf9f54b4759ed556392659ff
Albert Zhemukov <aazhemukov@edu.hse.ru>
2024-10-22 20:25:13 +0300

    Schemas updated, tests for the added schemas features
    1. TagBase, TagCreate, Tag and HackathonBase classes.
    2. Tests for tag creation.

--- backend/app/database/schemas.py
+++ backend/app/database/schemas.py
@@ -19,3 +19,27 @@ class UserEdit(UserBase):
 
 class User(UserBase):
     id: int
+
+
+class TagBase(BaseModel):
+    name: str
+    transparency: float = 1.0
+    color: str = "rgba(255, 87, 51, transparency)"
+
+
+class TagCreate(TagBase):
+    pass
+
+
+class Tag(TagBase):
+    id: int
+    users: list[User] = []
+
+    class Config:
+        orm_mode = True
+        schema_extra = {}
+
+
+class HackathonBase(BaseModel):
+    name: str
+    description: str
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,22 @@
-from math import sin, cos
+from pydantic import ValidationError
+import pytest
+from .database.schemas import TagCreate, Tag
 
 
-def my_function(x: int) -> float:
-    return sin(x) * cos(x)
+def test_tag_create():
+    """Создает тег"""
+    tag = TagCreate(color="#FF0000", name="Test Tag")
+    assert tag.color == "#FF0000"
+    assert tag.name == "Test Tag"
+
+
+def test_tag_create_rgba():
+    """Проверяет создание тега с цветом палитры rgba"""
+    tag = TagCreate(color="rgba(255, 87, 51, 0.5)", transparency=0.5, name="Test Tag")
+    assert tag.color == "rgba(255, 87, 51, 0.5)"
+    assert tag.transparency == 0.5
+
+
+def test_tag_invalid_color():
+    with pytest.raises(ValidationError):
+        TagCreate(color="12345", name="Invalid Color")
