667aa5cb945a75afae842f0a1217e92fa4f9f6fc
Maxim Drugov <midrugov@edu.hse.ru>
2024-11-06 23:45:52 +0300

    basic implementation of main tables
    added tables for user, icon, team and some additional tables for it

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,7 +1,10 @@
+from typing import Optional, List
+
+from pydantic import EmailStr
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped
 from datetime import datetime, timezone
 
 Base = declarative_base()
@@ -19,150 +22,110 @@ class BaseModel(Base):
     )
 
 
+class ManyToManyBase(Base):
+    __abstract__ = True
+
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
+
+
 class User(BaseModel):
-    __table_name__ = "user"
+    __tablename__ = "user"
 
-    email = Column(String, unique=True, index=True, nullable=False)
-    first_name = Column(String, nullable=False)
-    second_name = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_admin = Column(Boolean, default=False)
+    email: EmailStr = Column(String, unique=True, nullable=False)
+    first_name: str = Column(String, nullable=False)
+    second_name: str = Column(String, nullable=False)
+    hashed_password: str = Column(String, nullable=False)
+    role: str = Column(String, nullable=False, default="user")
+    link_cv: str = Column(String, nullable=False)
 
-    tags = relationship("UserTag", back_populates="user", lazy="joined")
-    hackathons = relationship(
-        "OrganizerHackathon", back_populates="user", lazy="joined"
-    )
+    teams: Mapped[Optional["Team"]] = relationship("Team", secondary="TeamUser", back_populates="users")
 
 
-class UserTag(Base):
-    __table_name__ = "user_tag"
+class Icon(BaseModel):
+    __tablename__ = "icon"
 
-    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    tag_id = Column(Integer, ForeignKey("tag.id"), primary_key=True)
+    title: str = Column(String, nullable=False, unique=True)
+    icon_link: str = Column(String, nullable=False)
 
-    user = relationship("User", back_populates="tags")
-    tag = relationship("Tag", back_populates="users")
-
-
-class Tag(BaseModel):
-    __table_name__ = "tag"
-
-    title = Column(String, nullable=False)
-    color = Column(String, default="#777777")
-
-    users = relationship("UserTag", back_populates="tag", lazy="joined")
-    tasks = relationship("TaskTag", back_populates="tag", lazy="joined")
+    teams: Mapped[Optional["Team"]] = relationship("Team", secondary="TeamIconSocial", back_populates="icons")
 
 
-class Hackathon(BaseModel):
-    __table_name__ = "hackathon"
+class Team(BaseModel):
+    __tablename__ = "team"
 
-    title = Column(String, nullable=False)
-    description = Column(Text)
-    prize = Column(Text)
-    status_id = Column(Integer, ForeignKey("hackathon_status.id"))
-    redirect_link = Column(String)
-    poster_link = Column(String)
+    title: str = Column(String, nullable=False)
+    description: str = Column(Text, nullable=False)
+    icon_link: str = Column(String, nullable=False)
+    captain_id: int = Column(Integer, ForeignKey("user.id"), nullable=False)
+    is_looking_for_members: bool = Column(Boolean, nullable=False, default=False)
 
-    tasks = relationship("HackathonTask", back_populates="hackathon", lazy="joined")
-    tags = relationship("HackathonTag", back_populates="hackathon", lazy="joined")
-    organizers = relationship(
-        "OrganizerHackathon", back_populates="hackathon", lazy="joined"
-    )
-    status = relationship("HackathonStatus", back_populates="hackathons", lazy="joined")
-
-
-class HackathonTask(Base):
-    __table_name__ = "hackathon_task"
-
-    hackathon_id = Column(Integer, ForeignKey("hackathon.id"), primary_key=True)
-    task_id = Column(Integer, ForeignKey("task.id"), primary_key=True)
-
-    hackathon = relationship("Hackathon", back_populates="tasks")
-    task = relationship("Task", back_populates="hackathons")
+    users: Mapped[List["User"]] = relationship("User", secondary="TeamUser", back_populates="teams")
+    icons: Mapped[Optional[List["Icon"]]] = relationship("Icon", secondary="TeamIconSocial", back_populates="teams")
+    common_tags: Mapped[Optional[List["CommonTag"]]] = relationship("CommonTag", secondary="TeamCommonTag",
+                                                                    back_populates="teams")
+    field_tags: Mapped[Optional[List["FieldTag"]]] = relationship("FieldTag", secondary="TeamFieldTag",
+                                                                  back_populates="teams")
 
 
-class Task(BaseModel):
-    __table_name__ = "task"
+class TeamRole(BaseModel):
+    __tablename__ = "team_role"
 
-    title = Column(String, nullable=False)
-    description = Column(Text)
-
-    hackathons = relationship("HackathonTask", back_populates="task", lazy="joined")
-    tags = relationship("TaskTag", back_populates="task", lazy="joined")
+    title: str = Column(String, nullable=False)
 
 
-class TaskTag(Base):
-    __table_name__ = "task_tag"
+class TeamUser(ManyToManyBase):
+    __tablename__ = "team_user"
 
-    task_id = Column(Integer, ForeignKey("task.id"), primary_key=True)
-    tag_id = Column(Integer, ForeignKey("tag.id"), primary_key=True)
+    team_id: int = Column(Integer, ForeignKey("team.id"), primary_key=True, nullable=False)
+    user_id: int = Column(Integer, ForeignKey("user.id"), primary_key=True, nullable=False)
+    is_accepted: bool = Column(Boolean, nullable=False, default=False)
+    team_role_id: int = Column(Integer, ForeignKey("team_role.id"), nullable=False)
 
-    task = relationship("Task", back_populates="tags")
-    tag = relationship("Tag", back_populates="tasks")
-
-
-class OrganizerHackathon(Base):
-    __table_name__ = "organizer_hackathon"
-
-    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    hackathon_id = Column(Integer, ForeignKey("hackathon.id"), primary_key=True)
-
-    user = relationship("User", back_populates="hackathons")
-    hackathon = relationship("Hackathon", back_populates="organizers")
+    team_role: Mapped[Optional[TeamRole]] = relationship("TeamRole")
 
 
-class HackathonType(BaseModel):
-    __table_name__ = "hackathon_type"
+class TeamIconSocial(ManyToManyBase):
+    __tablename__ = "team_icon_social"
 
-    title = Column(String, nullable=False)
+    icon_id: int = Column(Integer, ForeignKey("icon.id"), primary_key=True, nullable=False)
+    team_id: int = Column(Integer, ForeignKey("team.id"), primary_key=True, nullable=False)
 
-    hackathons = relationship("HackathonTypeLink", back_populates="type", lazy="joined")
-
-
-class HackathonTypeLink(Base):
-    __table_name__ = "hackathon_type_link"
-
-    hackathon_id = Column(Integer, ForeignKey("hackathon.id"), primary_key=True)
-    type_id = Column(Integer, ForeignKey("hackathon_type.id"), primary_key=True)
-
-    hackathon = relationship("Hackathon", back_populates="types")
-    type = relationship("HackathonType", back_populates="hackathons")
+    icon: Mapped["Icon"] = relationship("Icon")
+    team: Mapped["Team"] = relationship("Team")
 
 
-class HackathonLocation(BaseModel):
-    __table_name__ = "hackathon_location"
+class CommonTag(BaseModel):
+    __tablename__ = "common_tag"
 
-    title = Column(String, nullable=False)
+    title: str = Column(String, nullable=False)
+    color: str = Column(String, nullable=False, default="#000000")
 
-    hackathons = relationship(
-        "HackathonLocationLink", back_populates="location", lazy="joined"
-    )
-
-
-class HackathonLocationLink(Base):
-    __table_name__ = "hackathon_location_link"
-
-    hackathon_id = Column(Integer, ForeignKey("hackathon.id"), primary_key=True)
-    location_id = Column(Integer, ForeignKey("hackathon_location.id"), primary_key=True)
-
-    hackathon = relationship("Hackathon", back_populates="locations")
-    location = relationship("HackathonLocation", back_populates="hackathons")
+    teams: Mapped[Optional[List["Team"]]] = relationship("Team", secondary="TeamCommonTag", back_populates="common_tags")
 
 
-class HackathonStatus(BaseModel):
-    __table_name__ = "hackathon_status"
+class FieldTag(BaseModel):
+    __tablename__ = "field_tag"
 
-    title = Column(String, nullable=False)
+    title: str = Column(String, nullable=False)
+    color: str = Column(String, nullable=False, default="#000000")
 
-    hackathons = relationship("Hackathon", back_populates="status", lazy="joined")
+    teams: Mapped[Optional[List["Team"]]] = relationship("Team", secondary="TeamFieldTag", back_populates="field_tags")
 
 
-class HackathonTag(Base):
-    __table_name__ = "hackathon_tag"
+class TeamCommonTag(ManyToManyBase):
+    __tablename__ = "team_common_tag"
 
-    hackathon_id = Column(Integer, ForeignKey("hackathon.id"), primary_key=True)
-    tag_id = Column(Integer, ForeignKey("tag.id"), primary_key=True)
+    team_id: int = Column(Integer, ForeignKey("team.id"), primary_key=True, nullable=False)
+    common_tag_id: int = Column(Integer, ForeignKey("common_tag.id"), primary_key=True, nullable=False)
 
-    hackathon = relationship("Hackathon", back_populates="tags")
-    tag = relationship("Tag", back_populates="hackathons")
+
+class TeamFieldTag(ManyToManyBase):
+    __tablename__ = "team_field_tag"
+
+    team_id: int = Column(Integer, ForeignKey("team.id"), primary_key=True, nullable=False)
+    field_tag_id: int = Column(Integer, ForeignKey("field_tag.id"), primary_key=True, nullable=False)
