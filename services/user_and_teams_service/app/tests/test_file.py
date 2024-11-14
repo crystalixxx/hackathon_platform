fe5359c79f72c33414f8dd6668cb9f280c84c68a
John Doe <101018980+zhemalb@users.noreply.github.com>
2024-11-14 13:52:31 +0300

    Placeholder test added in test_file.py

--- services/user_and_teams_service/app/tests/test_file.py
+++ services/user_and_teams_service/app/tests/test_file.py
@@ -1,8 +1,3 @@
-# from ..database.schemas import TagCreate
-#
-#
-# def test_tag_create():
-#     """Создает тег"""
-#     tag = TagCreate(color="#FF0000", name="Test Tag")
-#     assert tag.color == "#FF0000"
-#     assert tag.name == "Test Tag"
+def placeholder():
+    """placeholder"""
+    pass
