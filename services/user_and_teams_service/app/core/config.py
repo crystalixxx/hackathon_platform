1bae40a6d763ca8b07b944a81ac6e7ebae5badc6
Maxim Drugov <midrugov@edu.hse.ru>
2024-11-15 00:05:19 +0300

    removed useless attribute from config.py

--- services/user_and_teams_service/app/core/config.py
+++ services/user_and_teams_service/app/core/config.py
@@ -4,7 +4,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     settings_config = SettingsConfigDict(env_prefix="backend_")
 
-    database_connection_url: str = ""
-
 
 config = Config()
