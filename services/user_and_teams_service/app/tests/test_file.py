db5c40594c8ef47e085461d8b8b3027ebdf7dbaf
John Doe <101018980+zhemalb@users.noreply.github.com>
2024-11-14 13:58:09 +0300

    test_file.py: a basic test has been added that checks if a new user can be created and every user field can be filled.

--- services/user_and_teams_service/app/tests/test_file.py
+++ services/user_and_teams_service/app/tests/test_file.py
@@ -1,3 +1,33 @@
-def placeholder():
-    """placeholder"""
-    pass
+import uuid
+from datetime import datetime
+from ..database.schemas.user import TUserCreate
+
+
+def test_user_create():
+    """Проверяет, что можно создать пользователя и что все поля заполнены корректно"""
+
+    user_data = {
+        "id": uuid.uuid4(),
+        "email": "test@example.com",
+        "first_name": "John",
+        "second_name": "Doe",
+        "hashed_password": "hashedpassword123",
+        "role": "admin",
+        "link_cv": "https://example.com/cv",
+        "created_at": datetime.datetime.now(datetime.UTC),
+        "updated_at": datetime.datetime.now(datetime.UTC),
+        # datetime.datetime.utcnow() is deprecated and scheduled for removal in a
+        # future version. Use timezone-aware objects to represent datetime in UTC: datetime.datetime.now(datetime.UTC).
+    }
+
+    user = TUserCreate(**user_data)
+
+    assert user.id == user_data["id"]
+    assert user.email == user_data["email"]
+    assert user.first_name == user_data["first_name"]
+    assert user.second_name == user_data["second_name"]
+    assert user.hashed_password == user_data["hashed_password"]
+    assert user.role == user_data["role"]
+    assert user.link_cv == user_data["link_cv"]
+    assert user.created_at == user_data["created_at"]
+    assert user.updated_at == user_data["updated_at"]
