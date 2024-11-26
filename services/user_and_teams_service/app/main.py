814cff68897308b779e316cac9a25c96bdbd0871
Maxim Drugov <midrugov@edu.hse.ru>
2024-11-26 14:49:15 +0300

    added some metadata into FastAPI class

--- services/user_and_teams_service/app/main.py
+++ services/user_and_teams_service/app/main.py
@@ -1,11 +1,12 @@
-from app.database.models.base import Base
+from api.v0.main import main_v0_router
+from database.models.base import Base
 from fastapi import FastAPI
 from uvicorn import run
 
 metadate = Base.metadata
 
-app = FastAPI()
-
+app = FastAPI(title="Hackathon Platform", docs_url="/api/docs", openapi_url="/api")
+app.include_router(main_v0_router)
 
 if __name__ == "__main__":
     run("main:app", host="0.0.0.0", reload=True, port=8000)
