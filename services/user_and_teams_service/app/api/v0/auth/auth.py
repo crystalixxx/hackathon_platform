35e4b10856d029a479ca7d67b08bdf12f866e83a
Maxim Drugov <midrugov@edu.hse.ru>
2024-11-26 14:50:09 +0300

    written basic api routers feature
    added auth.py with post routers for auth (login, sign up), main.py
    as collector of api routes of current api version and common for all
    api versions dependencies.py with some dependencies for routes

--- services/user_and_teams_service/app/api/v0/auth/auth.py
+++ services/user_and_teams_service/app/api/v0/auth/auth.py
@@ -1,8 +1,62 @@
-from fastapi import APIRouter, Request
+from datetime import timedelta
+from typing import Annotated
+
+from api.dependencies import UOWAlchemyDep
+from app.core.config import config
+from app.core.security import create_access_token
+from app.database.schemas.user import UserCreate
+from app.services.user import UserServices
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from starlette import status
 
 auth_router = APIRouter()
 
 
 @auth_router.post("/sign_up")
-async def sign_up(request: Request):
-    return {"status": "ok"}
+async def sign_up(uow: UOWAlchemyDep, user: UserCreate):
+    user_dict = user.model_dump(exclude_none=True)
+    del user_dict["role"]
+
+    new_user = await UserServices().sign_up_new_user(uow, **user_dict)
+
+    if not new_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Account already exists",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    access_token = create_access_token(
+        data={"sub": new_user.email, "permissions": "user"},
+        expires_delta=access_token_expires,
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@auth_router.post("/login")
+async def login(
+    uow: UOWAlchemyDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = await UserServices().authenticate_user(
+        uow, form_data.username, form_data.password
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    access_token = create_access_token(
+        data={"sub": user.email, "permissions": "user"},
+        expires_delta=access_token_expires,
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
--- services/user_and_teams_service/app/api/v0/main.py
+++ services/user_and_teams_service/app/api/v0/main.py
@@ -1,5 +1,5 @@
-from auth.auth import auth_router
+from api.v0.auth.auth import auth_router
 from fastapi import APIRouter
 
-main_v0_router = APIRouter(prefix="/v0")
+main_v0_router = APIRouter(prefix="/api/v0")
 main_v0_router.include_router(auth_router, prefix="/auth")
