396524109db225cd348d08a114ced00ad12880ef
Maxim Drugov <midrugov@edu.hse.ru>
2024-11-26 14:42:54 +0300

    removed core/auth.py and past functions into user service as it's methods
    for logic and consistency of features postponed functions from auth.py into
    UserService class (they're working only with users data)

--- services/user_and_teams_service/app/services/user.py
+++ services/user_and_teams_service/app/services/user.py
@@ -1,7 +1,11 @@
-from app.core.security import get_hashed_password
+from app.core.config import config
+from app.core.security import get_hashed_password, oauth2_scheme, verify_password
 from app.core.utils.unit_of_work import AbstractUnitOfWork
 from app.database.schemas.user import UserCreate, UserUpdate
 from app.database.schemas.user_tag import UserTagCreate, UserTagUpdate
+from fastapi import Depends, HTTPException
+from jwt import DecodeError, decode
+from starlette import status
 
 
 class UserService:
@@ -12,8 +16,57 @@ class UserService:
 
         async with uow:
             user_id = await uow.users.add_one(user_dict)
-            await uow.commit()
+
             return user_id
+
+    async def sign_up_new_user(self, uow: AbstractUnitOfWork, user: UserCreate):
+        user = await self.get_user_by_email(uow, user.email)
+
+        if user:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="User with this email already exists",
+            )
+
+        new_user = await self.create_user(uow, user)
+        return new_user
+
+    async def authenticate_user(
+        self, uow: AbstractUnitOfWork, email: str, password: str
+    ):
+        user = await self.get_user_by_email(uow, email)
+
+        if not user:
+            return False
+
+        if not verify_password(password, user.hashed_password):
+            return False
+
+        return True
+
+    async def get_current_user(
+        self, uow: AbstractUnitOfWork, token: str = Depends(oauth2_scheme)
+    ):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+        try:
+            payload = decode(token, config.SECURITY_KEY, algorithms=[config.ALGORITHM])
+            email = payload.get("sub")
+
+            if email is None:
+                raise credentials_exception
+        except DecodeError:
+            raise credentials_exception
+
+        user = await self.get_user_by_email(uow, email)
+        if user is None:
+            raise credentials_exception
+
+        return user
 
     async def get_users(self, uow: AbstractUnitOfWork):
         async with uow:
@@ -48,6 +101,7 @@ class UserService:
 
         async with uow:
             user = await uow.users.update({"id": user_id}, user_dict)
+
             return user
 
     async def delete_user(self, uow: AbstractUnitOfWork, user_id: int):
@@ -58,6 +112,7 @@ class UserService:
 
         async with uow:
             user = await uow.users.delete({"id": user_id})
+
             return user
 
     async def create_user_tag(self, uow: AbstractUnitOfWork, user_tag: UserTagCreate):
@@ -65,11 +120,13 @@ class UserService:
 
         async with uow:
             user_tag = await uow.user_tags.add_one(user_tag_dict)
+
             return user_tag
 
     async def get_user_tags(self, uow: AbstractUnitOfWork, user_id: int):
         async with uow:
             user_tags = await uow.user_tags.find_some({"user_id": user_id})
+
             return user_tags
 
     async def get_user_tag_by_name(
@@ -98,6 +155,7 @@ class UserService:
             user_tag = await uow.user_tags.update(
                 {"user_id": user_id, "name": tag_name}, user_tag_dict
             )
+
             return user_tag
 
     async def delete_user_tag(
@@ -112,4 +170,5 @@ class UserService:
             user_tag = await uow.user_tags.delete(
                 {"user_id": user_id, "name": tag_name}
             )
+
             return user_tag
