94f30535aa8ba81a58e69ef0656634d88ce8cc93
John Doe <101018980+zhemalb@users.noreply.github.com>
2024-11-27 22:18:03 +0300

    Removed Redis config placeholder

--- services/user_and_teams_service/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-    REDIS_DB = int(os.getenv("REDIS_DB", 0))
