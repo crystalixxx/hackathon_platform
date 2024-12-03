9716ad7a500407c87b431bb6ececaef14e1c0aff
John Doe <101018980+zhemalb@users.noreply.github.com>
2024-12-03 22:47:35 +0300

    Updated User and Team repositories:
    1. Major TeamRepository update with Redis caching support.
    2. Simplified TeamRepository and UserRepository by removing redundant methods.

--- services/user_and_teams_service/app/repositories/team.py
+++ services/user_and_teams_service/app/repositories/team.py
@@ -1,6 +1,55 @@
-from app.core.utils import SQLAlchemyRepository
+from app.core.redis_client import RedisClient
+from app.core.utils.repository import SQLAlchemyRepository
 from app.database.models.team import Team
 
 
 class TeamRepository(SQLAlchemyRepository):
     model = Team
+
+    def __init__(self):
+        super().__init__()
+        self.redis_client = RedisClient()
+
+    # async def create_team(self, team_dict: dict):
+    # team = await super().add_one(team_dict)
+    # return team
+
+    # async def get_teams(self):
+    # teams = await super().find_all()
+    # return teams
+
+    async def get_team_by_id(self, team_id: int):
+        cache_key = f"team:{team_id}"
+        cached_team = self.redis_client.get(cache_key)
+
+        if cached_team:
+            return cached_team
+
+        team = await super().find_one({"id": team_id})
+
+        if team:
+            self.redis_client.set(cache_key, team, expire=120)
+
+        return team
+
+    # async def get_team_by_name(self, team_name: str):
+    # team = await super().find_one({"name": team_name})
+    # return team
+
+    async def update_team(self, team_id: int, update_dict: dict):
+        team = await super().update({"id": team_id}, update_dict)
+
+        if team:
+            cache_key = f"team:{team_id}"
+            self.redis_client.delete(cache_key)
+
+        return team
+
+    async def delete_team(self, team_id: int):
+        team = await super().delete({"id": team_id})
+
+        if team:
+            cache_key = f"team:{team_id}"
+            self.redis_client.delete(cache_key)
+
+        return team
