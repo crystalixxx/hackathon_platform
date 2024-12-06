3e56aadb359da92fe5fa0a5cf60b2f849761ae65
John Doe <101018980+zhemalb@users.noreply.github.com>
2024-12-06 17:07:41 +0300

    Added request handling logic to RequestService:
    1. Implemented get_requests_sent_by_team to fetch team-sent requests.
    
    2. Implemented get_requests_to_team to fetch requests directed at the team.
    
    3. Added approve_request for marking requests as approved.
    
    4. Added reject_request to handle request rejection and deletion.

--- services/user_and_teams_service/app/services/request.py
+++ services/user_and_teams_service/app/services/request.py
@@ -1,5 +1,6 @@
 from app.core.utils.unit_of_work import AbstractUnitOfWork
 from app.database.schemas.request import RequestCreate
+from fastapi import HTTPException, status
 
 
 class RequestService:
@@ -25,6 +26,20 @@ class RequestService:
             request = uow.request.find_one({"team_id": team_id})
             return request
 
+    async def get_requests_sent_by_team(self, uow: AbstractUnitOfWork, team_id: int):
+        async with uow:
+            requests = await uow.request.find_all(
+                {"request_team_id": team_id, "sent_by_team": True}
+            )
+            return requests
+
+    async def get_requests_to_team(self, uow: AbstractUnitOfWork, team_id: int):
+        async with uow:
+            requests = await uow.request.find_all(
+                {"request_team_id": team_id, "sent_by_team": False}
+            )
+            return requests
+
     async def delete_request(self, uow: AbstractUnitOfWork, request_id):
         request = self.get_request_by_id(uow, request_id)
 
@@ -34,3 +49,31 @@ class RequestService:
         async with uow:
             request = uow.request.delete({"id": request_id})
             return request
+
+    async def approve_request(self, uow: AbstractUnitOfWork, request_id: int):
+        async with uow:
+            request = await self.get_request_by_id(uow, request_id)
+
+            if not request:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Реквест #{request_id} не найден.",
+                )
+
+            updated_request = await uow.request.update(
+                {"id": request_id}, {"is_ok": True}
+            )
+            return updated_request
+
+    async def reject_request(self, uow: AbstractUnitOfWork, request_id: int):
+        async with uow:
+            request = await self.get_request_by_id(uow, request_id)
+
+            if not request:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Реквест #{request_id} не найден.",
+                )
+
+            await uow.request.delete({"id": request_id})
+            return {"message": f"Реквест #{request_id} отклонен."}
