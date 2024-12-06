19c41b43dc8920125cedcd34552b3c0a62e7e4f4
John Doe <101018980+zhemalb@users.noreply.github.com>
2024-12-06 14:19:14 +0300

    Removed unused import in dependencies.py

--- services/user_and_teams_service/app/api/dependencies.py
+++ services/user_and_teams_service/app/api/dependencies.py
@@ -2,6 +2,9 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from app.core.utils.unit_of_work import AbstractUnitOfWork, SQLAlchemyUnitOfWork, CachedSQLAlchemyUnitOfWork
+from app.core.utils.unit_of_work import (
+    AbstractUnitOfWork,
+    CachedSQLAlchemyUnitOfWork,
+)
 
 UOWAlchemyDep = Annotated[AbstractUnitOfWork, Depends(CachedSQLAlchemyUnitOfWork)]
