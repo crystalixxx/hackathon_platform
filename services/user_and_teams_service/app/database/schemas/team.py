4f67823b84a67d5252c0a3c288f0004f25f772cd
Maxim Drugov <midrugov@edu.hse.ru>
2024-12-07 21:14:35 +0300

    changed wrong base class into team_user repo & created converting schema for it

--- services/user_and_teams_service/app/database/schemas/team.py
+++ services/user_and_teams_service/app/database/schemas/team.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
 
+from app.database.schemas.user import UserSchema
+
 
 class TeamBase(BaseModel):
     title: str
@@ -17,13 +19,23 @@ class TeamCreate(TeamBase):
 
 
 class TeamUpdate(BaseModel):
-    title: Optional[str]
-    description: Optional[str]
-    icon_url: Optional[str]
-    is_looking_for_members: Optional[bool]
-    captain_id: Optional[int]
+    title: Optional[str] = None
+    description: Optional[str] = None
+    icon_url: Optional[str] = None
+    is_looking_for_members: Optional[bool] = None
+    captain_id: Optional[int] = None
 
 
 class TeamSchema(TeamBase):
     id: int
     captain_id: int
+
+
+class TeamUserSchema(BaseModel):
+    id: int
+    team_id: int
+    user_id: int
+    role_name: str
+    user: UserSchema
+
+    model_config = ConfigDict(from_attributes=True)
