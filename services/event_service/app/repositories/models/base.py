29523987b64aa3a8f8d1ccbc0f346f2b73813d51
John Doe <101018980+zhemalb@users.noreply.github.com>
2024-12-14 21:20:41 +0300

    added models for event service, still working on it

--- services/event_service/app/repositories/models/base.py
+++ services/event_service/app/repositories/models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, func
+from sqlalchemy import Column, DateTime, Integer, func
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
