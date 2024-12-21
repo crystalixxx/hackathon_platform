05fef10c6787ceba0a35af95ef90f944a874681d
Albert Zhemukov <aazhemukov@edu.hse.ru>
2024-12-21 21:10:06 +0300

    repositories implemented

--- services/event_service/app/repositories/event_prize.py
+++ services/event_service/app/repositories/event_prize.py
@@ -2,5 +2,5 @@ from app.core.utils.repository import CachedRepository
 from app.database.models.request import Request
 
 
-class AcceptedRepository(CachedRepository):
+class EventPrizeRepository(CachedRepository):
     model = Request
