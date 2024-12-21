ae079f0a241f1de5dfa16e2c85fdaf563b437644
Albert Zhemukov <aazhemukov@edu.hse.ru>
2024-12-21 21:39:40 +0300

    unit of work implemented

--- services/event_service/app/core/unit_of_work.py
+++ services/event_service/app/core/unit_of_work.py
@@ -9,13 +9,22 @@ from app.core.utils.cache import RedisCache
 from app.core.utils.repository import AbstractRepository
 from app.database.redis import RedisSingleton
 from app.database.session import get_db
-# from app.repositories import (
-    # RequestRepository,
-    # TeamRepository,
-    # TeamUserRepository,
-    # UserRepository,
-    # UserTagRepository,
-# )
+from app.repositories import (
+    AcceptedRepository,
+    DateRepository,
+    EventRepository,
+    EventLocationRepository,
+    EventPrizeRepository,
+    LocationRepository,
+    StatusRepository,
+    StatusEventRepository,
+    StatusTrackRepository,
+    TimelineRepository,
+    TimelineStatusRepository,
+    TrackRepository,
+    TrackTeamRepository,
+    TrackWinnersRepository
+)
 
 
 class AbstractUnitOfWork(ABC):
@@ -56,11 +65,19 @@ class CachedSQLAlchemyUnitOfWork(AbstractUnitOfWork):
     async def __aenter__(self):
         self.session: AsyncSession = await anext(self.session_factory())
 
-        # self.users = self.get_repository(UserRepository)
-        # self.team = self.get_repository(TeamRepository)
-        # self.request = self.get_repository(RequestRepository)
-        # self.user_tags = self.get_repository(UserTagRepository)
-        # self.team_user = self.get_repository(TeamUserRepository)
+        self.accepted_team = self.get_repository(AcceptedRepository)
+        self.date = self.get_repository(DateRepository)
+        self.event = self.get_repository(EventRepository)
+        self.event_location = self.get_repository(EventLocationRepository)
+        self.location = self.get_repository(LocationRepository)
+        self.status = self.get_repository(StatusRepository)
+        self.status_event = self.get_repository(StatusEventRepository)
+        self.status_track = self.get_repository(StatusTrackRepository)
+        self.timeline = self.get_repository(TimelineRepository)
+        self.timeline_status = self.get_repository(TimelineStatusRepository)
+        self.track = self.get_repository(TrackRepository)
+        self.track_team = self.get_repository(TrackTeamRepository)
+        self.track_winners = self.get_repository(TrackWinnersRepository)
 
         return await super().__aenter__()
 
