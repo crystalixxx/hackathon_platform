13444a21cdc6b569ed58342e157ec53f4e99b06c
Albert Zhemukov <aazhemukov@edu.hse.ru>
2024-12-23 10:51:40 +0300

    removed unnecessary files

--- services/event_service/app/tests/date.py
+++ services/event_service/app/tests/date.py
@@ -7,15 +7,6 @@ from fastapi import HTTPException
 from services.event_service.app.database.schemas.date import DateSchema
 
 
-@pytest.fixture
-def mock_uow():
-    uow = MagicMock()
-    uow.team = AsyncMock()
-    uow.team_user = AsyncMock()
-    uow.users = AsyncMock()
-    return uow
-
-
 def test_date_basic():
     data = {"date_start": "01.01.2025", "date_end": "01.01.2026"}
 
